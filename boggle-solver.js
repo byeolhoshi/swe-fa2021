f55f976d2897535c9df7dba0ca0433918b090e94
byeolhoshi <asmith@karat.com>
2021-10-13 22:33:43 -0500

    fix bug

--- boggle-solver.js
+++ boggle-solver.js
@@ -1,5 +1,7 @@
 //Nyhriel Smith, @02915670
-// Legland Burge, Source[code basis]
+// geekforgeeks.com trie javascript input
+// Dion Simmons
+
 exports.findAllSolutions = function(grid, dic) {
   let solutions = [];
 
@@ -32,7 +34,8 @@ exports.findAllSolutions = function(grid, dic) {
   solutions = Array.from(solutionSet);
   
   return solutions;
-    function isValid(grid){
+
+  function isValid(grid){
     guide = /(st|qu) | [a-prt-z]/;
     for (let i=0; i < grid.length; i++){
       for (let j=0; j<grid[i].length; j++){
@@ -60,7 +63,10 @@ exports.findAllSolutions = function(grid, dic) {
   
   function gridSearch(found, y, x, grid, visited, hash, solutionSet){
     let adjacents = [[-1,-1], [-1,0], [-1,1], [0,1], [1,1], [1,0], [1, -1], [0, -1]];
-    
+
+		if(found == "s" || found == "q"){
+			return;
+    }
     if (y<0 || x<0 || y>=grid.length || x>=grid.length || visited[y][x] == true){
       return;
     }
@@ -75,9 +81,6 @@ exports.findAllSolutions = function(grid, dic) {
           for (let i = 0; i < found.length; i++ ){
             if (i != (found.length-1)){
               continue;
-            }
-            else if(found[i] =='q' || found[i-1]=='q'){
-              break;
             }
             else{
               solutionSet.add(found);
@@ -132,7 +135,7 @@ var grid = [['T', 'W', 'Y', 'R'],
               ['O', 'N', 'T', 'A']];
 var dic = ['art', 'ego', 'gent', 'get', 'net', 'new', 'newt', 'prat',
                     'pry', 'qua', 'quart', 'quartz', 'rat', 'tar', 'tarp',
-                    'ten', 'went', 'wet', 'arty', 'egg', 'not', 'quar', 'ra'];
+                    'ten', 'went', 'wet', 'arty', 'egg', 'not', 'quar', 'ra', 'aqu'];
 
 console.log("Solutions: \n");
 
